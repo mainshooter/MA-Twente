dbdb15c5071f7e40e5aaacf230f2eec28e24732a
Peter 4A5A <498883@edu.rocmn.nl>
2017-04-07 09:43:52 +0200

    added user.class.php

---
+++ classes/user.class.php
@@ -0,0 +1,21 @@
+<?php
+
+  class user {
+    public var $email;
+    private var $password;
+
+
+
+    public function login($email, $password) {
+
+    }
+    private function verifyLogin() {
+
+    }
+    public function createUser() {
+
+    }
+
+  }
+
+?>
