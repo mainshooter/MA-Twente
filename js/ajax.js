18e290894a32a1915fd7895fbc595a81726deb40
Peter 4A5A <498883@edu.rocmn.nl>
2017-04-07 13:04:55 +0200

    Quick

---
+++ js/ajax.js
@@ -0,0 +1,11 @@
+function loadItem() {
+  // Ajax GET REQUEST
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      callBack(this);
+    }
+  };
+  xhttp.open("GET", fileLocation, true);
+  xhttp.send();
+}
