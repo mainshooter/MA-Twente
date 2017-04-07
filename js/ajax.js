fc0839ba365f129ec6b9c8aa884dd3c1a17fbce6
Peter 4A5A <498883@edu.rocmn.nl>
2017-04-07 13:25:55 +0200

    Added AJAX

--- js/ajax.js
+++ js/ajax.js
@@ -3,7 +3,8 @@ function loadItem(fileLocation) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      callBack(this);
+      // callBack(this);
+      document.getElementById('content').innerHTML = this.responseText;
     }
   };
   xhttp.open("GET", fileLocation, true);
