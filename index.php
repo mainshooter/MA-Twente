94d1ade0bf27ab4eabae9a0b150835acc9d3d30a
Peter 4A5A <498883@edu.rocmn.nl>
2017-04-07 12:37:56 +0200

    Login page added

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Login MA-Twente</title>
+
+    <link rel="stylesheet" href="style/grid.css" type="text/css">
+    <link rel="stylesheet" href="style/style.css" type="text/css">
+
+
+  </head>
+  <body>
+    <h1 class="col-12 center-text">Login system MA-Twente</h1>
+
+    <div class="row">
+      <div class="col-3"></div>
+      <div class="col-6">
+        <form method="post">
+          <div>Email</div>
+          <input type="email" name='email' />
+          <div>Password</div>
+          <input type="password" name='password' />
+          <div></div>
+          <input type="submit" name="user" value="login">
+        </form>
+      </div>
+      <div class="col-3"></div>
+  </div>
+  <?php
+  if (ISSET($_POST['user'])) {
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/leerjaar2/MA-Twente/classes/user.class.php';
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/leerjaar2/MA-Twente/classes/view.class.php';
+
+    $user = new user();
+    $view = new view();
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $login = $user->login($email, $password);
+
+    if ($login == true) {
+      header('Location: dashboard.php');
+
+    }
+    else if ($login == false) {
+      $view->displayMessage("Not correct");
+    }
+  }
+  ?>
+
+  </body>
+</html>
