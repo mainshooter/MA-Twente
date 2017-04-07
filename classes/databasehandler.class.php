41f3385f82712a9db31ddf777a8616f21a2a176f
Peter 4A5A <498883@edu.rocmn.nl>
2017-04-07 10:30:33 +0200

    Fixed the constructor bug

--- classes/databasehandler.class.php
+++ classes/databasehandler.class.php
@@ -10,7 +10,7 @@ class DbHandler {
   var $conn;
 
 
-  public function ___construct($host, $database, $username, $password) {
+  function __construct($host, $database, $username, $password) {
     $this->host = $host;
     $this->username = $username;
     $this->passworde = $password;
