797a61dcf29c3999ae130fddf085f3be3b38ccf3
Peter 4A5A <498883@edu.rocmn.nl>
2017-04-11 09:51:47 +0200

    Added forgot ajax method

--- js/ajax.js
+++ js/ajax.js
@@ -10,3 +10,16 @@ function loadItem(fileLocation) {
   xhttp.open("GET", fileLocation, true);
   xhttp.send();
 }
+function passwordForgotRequest(email) {
+  var xhttp = new XMLHttpRequest();
+  console.log(email.value);
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById('result').innerHTML = this.responseText;
+    }
+  };
+  // contains te post values
+  xhttp.open("POST", "ctrl/ctrl.user.php?user=forgot&email=" + email.value, true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send();
+}
